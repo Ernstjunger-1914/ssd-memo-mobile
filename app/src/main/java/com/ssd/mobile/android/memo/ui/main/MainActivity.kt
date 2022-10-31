09eaa2b2813006fe1ccad88bf39d51bdd4903d2a
asdf <vot768@naver.com>
2022-10-31 15:26:12 +0900

    [init] 1. 안드로이드 프로젝트 생성

---
+++ app/src/main/java/com/ssd/mobile/android/memo/ui/main/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ssd.mobile.android.memo.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssd.mobile.android.memo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
